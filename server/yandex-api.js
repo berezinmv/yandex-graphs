07f68ca4c80d2126b84541838d197eb38ecef274
Maxim Berezin <berezin.mvdev@yahoo.com>
2017-01-31 22:22:25 +0500

    Add 'getSite' function

--- server/yandex-api.js
+++ server/yandex-api.js
@@ -4,14 +4,31 @@ function getRequestUrlForSites(token) {
   return `https://api-metrika.yandex.ru/management/v1/counters?oauth_token=${token}`;
 }
 
+
+function getRequestUrlForSite(token, siteId) {
+  return `https://api-metrika.yandex.ru/management/v1/counter/${siteId}?oauth_token=${token}&fields=name`;
+}
+
 module.exports.getSites = function (token) {
   return new Promise(function (resolve, reject) {
     request(getRequestUrlForSites(token), function (err, response, body) {
       if (err) {
         reject(err);
       } else {
-        resolve(JSON.parse(body));
+        resolve(body);
       }
     })
   })
 }
+
+module.exports.getSite = function (token, siteId) {
+  return new Promise(function (resolve, reject) {
+    request(getRequestUrlForSite(token, siteId), function (err, response, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+};
