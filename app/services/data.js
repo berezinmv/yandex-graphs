21e13c082cd682269838a167b470da2763b333b8
Maxim Berezin <berezin.mvdev@yahoo.com>
2017-02-01 12:29:17 +0500

    Parse response body

--- app/services/data.js
+++ app/services/data.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   getData: function (requestParameters) {
-    return $.post('/data', requestParameters);
+    return $.post('/data', {params: requestParameters}).then(JSON.parse);
   }
 });
