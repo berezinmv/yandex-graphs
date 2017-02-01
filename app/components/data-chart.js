35876937a2cf264cf8807693870a16ae08af9668
Maxim Berezin <berezin.mvdev@yahoo.com>
2017-02-01 12:29:33 +0500

    Add 'data-chart' component

---
+++ app/components/data-chart.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  chartOptions: Ember.computed('data', function () {
+    let data = this.get('data');
+    if (!data) {
+      return {};
+    }
+    let categories = data.time_intervals
+      .map(interval => interval
+        .map(date => new Date(date).toLocaleDateString('ru')).join(' - '));
+    console.log(categories);
+    return {
+      chart: {
+        type: 'spline'
+      },
+      xAxis: {
+        visible: false
+      }
+    };
+  }),
+
+  chartData: Ember.computed('data', function () {
+    let data = this.get('data');
+    if (!data) {
+      return [];
+    }
+    let metrics = data.data[0].metrics;
+    return metrics.map(metric => {
+      return {
+        data: metric
+      }
+    });
+  })
+});
